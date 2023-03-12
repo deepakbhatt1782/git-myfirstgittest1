05933248489eb876526189aa234c5b5e04e589a6
deepak <deepakbhatt1782@gmail.com>
2023-03-12 23:38:48 +0000

    Added new class

---
+++ src/GitFolderTest.java
@@ -0,0 +1,14 @@
+public class GitFolderTest {
+     static int a = 2;
+     static int b = 5;
+     static  int ans;
+     static void add(){
+       ans  = a+b;
+         System.out.println(ans);
+     }
+
+    public static void main(String[] args) {
+
+        add();
+    }
+}
